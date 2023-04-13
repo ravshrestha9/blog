82fbd2d02b9924edadcd4ee4b28dba6f56907607
Ravi <shrestharavee1@gmail.com>
2023-04-12 20:32:22 -0500

    Add moderation logic for comments to be accepted or rejected

--- moderation/index.js
+++ moderation/index.js
@@ -6,8 +6,22 @@ const app = express()
 app.use(bodyParser.json())
 
 
-app.get('/' , (req, res)=> {
+app.post('/events' , async(req, res)=> {
+    const {type , data } = req.body
+    if (type === 'CommentCreated'){
+        const status = data.content.includes('orange') ? 'rejected' : 'approved'
 
+        await axios.post('http://localhost:4005/events', {
+            type: 'CommentModerated',
+            data: {
+                id: data.id,
+                postId: data.postId,
+                content: data.content,
+                status
+            }
+        })
+    }
+    res.send({})
 })
 
 app.listen(4003, ()=> {
